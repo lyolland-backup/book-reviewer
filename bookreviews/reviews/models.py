c417874a6b71958e1aaf0100fca8267b51bcfb24
github username <lawrence@sciencedisrupt.com>
2019-10-20 17:43:42 +0100

    new django book reviewing project

--- bookreviews/reviews/models.py
+++ bookreviews/reviews/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Review(models.Model):
-    title = models.CharField(max_length=400, unique=True)
-    author = models.CharField(max_length=100)
-    review = models.CharField(max_length=1000)
-    create_at = models.DateTimeField(auto_now_add=True)
