e419c5f1ca5cfaa5fba0c5bff35cf03e157c3b82
github username <lawrence@sciencedisrupt.com>
2019-10-20 17:47:10 +0100

    fixed typo

--- bookreviews/reviews/models.py
+++ bookreviews/reviews/models.py
@@ -5,4 +5,4 @@ class Review(models.Model):
     title = models.CharField(max_length=400, unique=True)
     author = models.CharField(max_length=100)
     review = models.CharField(max_length=1000)
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
