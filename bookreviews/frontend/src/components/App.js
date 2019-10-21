9939dbb652566656eb5bb75547da2b3f07923e60
github username <lawrence@sciencedisrupt.com>
2019-10-21 10:21:03 +0100

    setting up redux

--- bookreviews/frontend/src/components/App.js
+++ bookreviews/frontend/src/components/App.js
@@ -2,16 +2,19 @@ import React, { Component, Fragment } from "react";
 import ReactDom from "react-dom";
 import Header from "./layout/Header";
 import ReviewDashboard from "./reviews/reviewDashboard";
-
+import { Provider } from "react-redux";
+import store from "../store";
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Header />
-        <div className="container">
-          <ReviewDashboard />
-        </div>
-      </Fragment>
+      <Provider store={store}>
+        <Fragment>
+          <Header />
+          <div className="container">
+            <ReviewDashboard />
+          </div>
+        </Fragment>
+      </Provider>
     );
   }
 }
